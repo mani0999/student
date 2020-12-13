4073dcee675f0879f894ae2777125a35b76c24b9
unknown <manikanth0999@gmail.com>
2020-12-13 22:18:37 +0000

    changed localhost to rds

--- database.js
+++ database.js
@@ -3,16 +3,16 @@ var mysql = require('mysql');
 
 //create connection to mysql
 var con = mysql.createConnection({
-  host: "database-1.c5ocoax9a06y.us-east-1.rds.amazonaws.com",
+  host: "database-2.c5ocoax9a06y.us-east-1.rds.amazonaws.com",
   port: "3306",
   user: "admin",
-  password: "mani1234",
-  database:"union"
+  password: "adminadmin",
+  database:"uniondb"
 });
 
 //connecting mysql database
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 module.exports = con;
