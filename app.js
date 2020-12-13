e4782639c0a7039604c99a330108b2927f89c5da
unknown <manikanth0999@gmail.com>
2020-12-13 15:20:12 +0000

    server chnage to 8081

--- app.js
+++ app.js
@@ -19,5 +19,5 @@ var usersRouter = require('./routes/users');
 app.use('/users',usersRouter);
 
 //local host runing port
-app.listen(8080); 
-console.log("Server running at Port 8080"); 
+app.listen(8081); 
+//console.log("Server running at Port 8080"); 
