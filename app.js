63f05897360066b59022b56641b3e916563a4972
unknown <manikanth0999@gmail.com>
2020-12-13 13:10:26 +0000

    changed the port

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ var usersRouter = require('./routes/users');
 app.use('/users',usersRouter);
 
 //local host runing port
-app.listen(3000); 
+app.listen(3360); 
 //console.log("Server running at Port 8080"); 
